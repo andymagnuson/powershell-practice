f9815610282b951e947a51d373232a99071faa76
Andy Magnuson <andymagnuson@gmail.com>
2020-08-10 11:07:00 -0400

    First attempt

---
+++ auto-commiting-setup.ps1
@@ -0,0 +1,10 @@
+$GitHubUsername = "andymagnuson"
+$CommitMessage = 'auto committed from auto-committing-setup.ps1!'
+$PracticeRepoDir= 'C:\Users\andy\source\repos\powershell-practice'
+git clone "https://github.com/$GitHubUsername/powershell-practice" "$PracticeRepoDir"
+Copy-Item C:\Users\andy\source\repos\auto-commiting-setup.ps1 C:\Users\andy\source\repos\powershell-practice
+cd $PracticeRepoDir
+git add --all
+git commit -m "First attempt"
+git push origin master
+
